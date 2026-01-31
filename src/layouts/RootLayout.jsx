d9c0cff08e372992b6b74b6d81fbd31e319c2c42
Bhadrayu09 <127506313+Bhadrayu09@users.noreply.github.com>
2026-01-31 21:23:05 +0530

    added some pages
    added footer, whatsapp button, images  done responsive

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import WhatsAppButton from '../components/WhatsAppButton'
 
 function RootLayout() {
   return (
@@ -12,6 +13,11 @@ function RootLayout() {
         <Outlet/>
       </main>
       <Footer/>
+       {/* Global floating WhatsApp button */}
+      <WhatsAppButton
+        phone="917483987568"
+        message="Hello! I'd like to know more about your yoga programs."
+      />
     </div>
   )
 }
